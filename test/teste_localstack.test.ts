d3622271d36e34bd8b641b8cfa29e8d9949833a1
Jholdy Damasceno <jholdy.dev@gmail.com>
2024-07-12 00:57:45 -0300

    Initial commit

---
+++ test/teste_localstack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TesteLocalstack from '../lib/teste_localstack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/teste_localstack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TesteLocalstack.TesteLocalstackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
