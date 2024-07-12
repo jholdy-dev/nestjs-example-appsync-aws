0d754feace03a89aa30fc23f821898ea9ea09c4c
Jholdy Damasceno <jholdy.dev@gmail.com>
2024-07-12 02:23:35 -0300

    chore: init commit

--- lib/teste_localstack-stack.ts
+++ lib/teste_localstack-stack.ts
@@ -1,16 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
-export class TesteLocalstackStack extends cdk.Stack {
+export class CdkLocalstackDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TesteLocalstackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new dynamodb.Table(this, "MyTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+    });
   }
 }
